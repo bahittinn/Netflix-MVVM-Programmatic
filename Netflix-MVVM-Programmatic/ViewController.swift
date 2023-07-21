2e2b4d478021ae9aee9e91f21d803d3a837ee22a
bahittinn <bahittin84@gmail.com>
2023-07-21 18:53:58 +0300

    Storyboard removed

--- Netflix-MVVM-Programmatic/ViewController.swift
+++ Netflix-MVVM-Programmatic/ViewController.swift
@@ -7,11 +7,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
+        
+        let vc1 = UINavigationController(rootViewController: HomeViewController())
+        let vc2 = UINavigationController(rootViewController: HomeViewController())
+        let vc3 = UINavigationController(rootViewController: HomeViewController())
+        let vc4 = UINavigationController(rootViewController: HomeViewController())
+        
+        vc1.tabBarItem.image = UIImage(systemName: "gear")
+        vc2.tabBarItem.image = UIImage(systemName: "gear")
+        vc3.tabBarItem.image = UIImage(systemName: "gear")
+        vc4.tabBarItem.image = UIImage(systemName: "gear")
+        
+        vc1.title = "anasayfa"
+        vc2.title = "anasayfa2"
+        vc3.title = "anasayfa3"
+        vc4.title = "anasayfa4"
+        
+        
+        setViewControllers([vc1,vc2,vc3,vc4], animated: true)
     }
 
 
