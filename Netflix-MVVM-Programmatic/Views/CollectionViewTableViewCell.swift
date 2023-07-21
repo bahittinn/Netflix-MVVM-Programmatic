ad83210f2ea71dd1aa433117230eb1017025438b
bahittinn <bahittin84@gmail.com>
2023-07-21 23:43:25 +0300

    Tableview Settings

---
+++ Netflix-MVVM-Programmatic/Views/CollectionViewTableViewCell.swift
@@ -0,0 +1,34 @@
+//
+//  CollectionViewTableViewCell.swift
+//  Netflix-MVVM-Programmatic
+//
+//  Created by Bahittin on 21.07.2023.
+//
+
+import UIKit
+
+class CollectionViewTableViewCell: UITableViewCell {
+    
+    static let identifier = "CollectionViewTableViewCell"
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        contentView.backgroundColor = .systemPink
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+        // Configure the view for the selected state
+    }
+    
+}
