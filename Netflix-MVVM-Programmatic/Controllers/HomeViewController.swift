4cfc5e3135b03e26ec9cb1c946e07fc8d3d6c291
bahittinn <bahittin84@gmail.com>
2023-07-21 23:33:17 +0300

    Home feed table

--- Netflix-MVVM-Programmatic/Controllers/HomeViewController.swift
+++ Netflix-MVVM-Programmatic/Controllers/HomeViewController.swift
@@ -8,23 +8,36 @@
 import UIKit
 
 class HomeViewController: UIViewController {
-
+    
+    private let homeFeedTable: UITableView = {
+        let table = UITableView()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return table
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Anasayfa"
-    
-        // Do any additional setup after loading the view.
+        view.addSubview(homeFeedTable)
+        
+        homeFeedTable.delegate = self
+        homeFeedTable.dataSource = self
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        homeFeedTable.frame = view.bounds
     }
-    */
+}
 
+extension HomeViewController: UITableViewDelegate,UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "deneme"
+        return cell
+    }
 }
