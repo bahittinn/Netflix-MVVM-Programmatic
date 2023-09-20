aefb62ea25ab1d169b612d41487fef1c2185143f
bahittinn <bahittin84@gmail.com>
2023-09-20 21:43:50 +0300

    SdWebImage installed for cell

---
+++ Netflix-MVVM-Programmatic/Views/TitleCollectionViewCell.swift
@@ -0,0 +1,40 @@
+//
+//  TitleCollectionViewCell.swift
+//  Netflix-MVVM-Programmatic
+//
+//  Created by Bahittin on 20.09.2023.
+//
+
+import UIKit
+import SDWebImage
+
+class TitleCollectionViewCell: UICollectionViewCell {
+    static let identifier = "TitleCollectionViewCell"
+    
+    private let posterImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.contentMode = .scaleAspectFill
+        return iv
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        contentView.addSubview(posterImageView)
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        posterImageView.frame = contentView.bounds
+    }
+    
+    public func configure(with model: String) {
+        let url = URL(string: model)
+        posterImageView.sd_setImage(with: url)
+    }
+    
+}
