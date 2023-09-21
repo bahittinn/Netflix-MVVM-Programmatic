54a682ed87c9f4507333677a63be56cdbd646184
bahittinn <bahittin84@gmail.com>
2023-09-21 10:17:16 +0300

    showing image in cell

--- Netflix-MVVM-Programmatic/Views/TitleCollectionViewCell.swift
+++ Netflix-MVVM-Programmatic/Views/TitleCollectionViewCell.swift
@@ -33,8 +33,8 @@ class TitleCollectionViewCell: UICollectionViewCell {
     }
     
     public func configure(with model: String) {
-        let url = URL(string: model)
-        posterImageView.sd_setImage(with: url)
+        guard let url = URL(string: "https://image.tmdb.org/t/p/w500/\(model)") else { return }
+        posterImageView.sd_setImage(with: url, completed: nil)
     }
     
 }
