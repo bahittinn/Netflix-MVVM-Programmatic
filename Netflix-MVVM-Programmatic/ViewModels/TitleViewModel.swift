3a187a29843c4f8e405453cf9e1c6e3c503adb42
bahittinn <bahittin84@gmail.com>
2023-09-21 21:16:51 +0300

    cell setup

---
+++ Netflix-MVVM-Programmatic/ViewModels/TitleViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  TitleViewModel.swift
+//  Netflix-MVVM-Programmatic
+//
+//  Created by Bahittin on 21.09.2023.
+//
+
+import Foundation
+
+struct TitleViewModel {
+    
+}
---
+++ Netflix-MVVM-Programmatic/Views/TitleTableViewCell.swift
@@ -0,0 +1,63 @@
+//
+//  TitleTableViewCell.swift
+//  Netflix-MVVM-Programmatic
+//
+//  Created by Bahittin on 21.09.2023.
+//
+
+import UIKit
+import SDWebImage
+
+class TitleTableViewCell: UITableViewCell {
+    
+    static let identifier = "TitleTableViewcell"
+    
+    private let playTitleButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    private let titlePosterUIImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        contentView.addSubview(titlePosterUIImageView)
+        contentView.addSubview(titleLabel)
+        contentView.addSubview(playTitleButton)
+    }
+    
+    private func applyConstraints() {
+        let titlePosterUIImageViewConstraints = [
+            titlePosterUIImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            titlePosterUIImageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 15),
+            titlePosterUIImageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -15),
+            titlePosterUIImageView.widthAnchor.constraint(equalToConstant: 100)
+        ]
+        
+        NSLayoutConstraint.activate(titlePosterUIImageViewConstraints)
+    }
+    
+    public func configure(with model: TitleViewModel) {
+        guard let url = URL(string: model.posterURL) else { return }
+        titlePosterUIImageView.sd_setImage(with: url, completed: nil)
+        titleLabel.text = model.titleName
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    
+}
