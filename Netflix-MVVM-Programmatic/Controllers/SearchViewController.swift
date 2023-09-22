20d8b73be82bd756947764e055031d421c6a34a3
bahittinn <bahittin84@gmail.com>
2023-09-22 20:45:20 +0300

    show data for searchviewcontroller

--- Netflix-MVVM-Programmatic/Controllers/SearchViewController.swift
+++ Netflix-MVVM-Programmatic/Controllers/SearchViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class SearchViewController: UIViewController {
     
+    private var titles : [Title] = [Title]()
+    
     private let discoverTable: UITableView = {
         let tableview = UITableView()
         tableview.register(TitleTableViewCell.self, forCellReuseIdentifier: TitleTableViewCell.identifier)
@@ -25,21 +27,46 @@ class SearchViewController: UIViewController {
         view.addSubview(discoverTable)
         discoverTable.delegate = self
         discoverTable.dataSource = self
+        
+        fetchDiscoverMovies()
+    }
+    
+    private func fetchDiscoverMovies() {
+        APICaller.shared.getDiscoverMovies { response in
+            switch response {
+            case .success(let titles):
+                self.titles = titles
+                DispatchQueue.main.async {
+                    self.discoverTable.reloadData()
+                }
+            case .failure(let error):
+                print("DEBUG: error is \(error.localizedDescription)")
+            }
+        }
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         discoverTable.frame = view.bounds
     }
+    
 }
 
 extension SearchViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return titles.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: TitleTableViewCell.identifier, for: indexPath) as? TitleTableViewCell else { return UITableViewCell() }
+        
+        let title = titles[indexPath.row]
+        let model = TitleViewModel(titleName: title.original_title ?? title.original_name ?? "Unknown Title", posterURL: title.poster_path ?? "Unknown")
+        cell.configure(with: model)
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 150
     }
 }
