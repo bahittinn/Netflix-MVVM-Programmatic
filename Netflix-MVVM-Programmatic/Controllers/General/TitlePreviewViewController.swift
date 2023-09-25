3d1671888b1e739f7a141d5c195d98f9abb134fa
bahittinn <bahittin84@gmail.com>
2023-09-25 21:41:16 +0300

    setup ui for title preview

--- Netflix-MVVM-Programmatic/Controllers/General/TitlePreviewViewController.swift
+++ Netflix-MVVM-Programmatic/Controllers/General/TitlePreviewViewController.swift
@@ -10,10 +10,49 @@ import WebKit
 
 class TitlePreviewViewController: UIViewController {
 
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = .systemFont(ofSize: 22, weight: .bold)
+        return label
+    }()
+    
+    private let overviewLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.font = .systemFont(ofSize: 18, weight: .regular)
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    private let downloadButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.backgroundColor = .red
+        button.setTitle("Download", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        return button
+    }()
+    
     private let webView: WKWebView = WKWebView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        view.addSubview(webView)
+        view.addSubview(titleLabel)
+        view.addSubview(overviewLabel)
+        view.addSubview(downloadButton)
+        
+        configureConstraints()
+    }
+    
+    private func configureConstraints() {
+        let webViewConstraints = [
+            webView.topAnchor.constraint(equalTo: view.topAnchor),
+            webView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            webView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            webView.heightAnchor.constraint(equalToConstant: 250)
+        ]
     }
 }
